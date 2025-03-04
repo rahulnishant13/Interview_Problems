cf47a8f73e23424a12156f5fdcd2858885945503
Rahul Nishant <rahulnishant13@gmail.com>
2025-03-04 15:47:34 +0530

    Create Trie.java

---
+++ Trie.java
@@ -0,0 +1,75 @@
+// implement Trie insert search and prefix search
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class TrieNode {
+    Map<Character, TrieNode> children;
+    boolean endOfWord;
+
+    TrieNode() {
+        children =  new HashMap<>();
+        endOfWord = false;
+    }
+}
+
+class Trie {
+    private TrieNode root;
+
+    Trie(){
+        root = new TrieNode();
+    }
+
+    void insert(String st) {
+        TrieNode curr = root;
+        for(char ch : st.toCharArray()) {
+            curr.children.putIfAbsent(ch, new TrieNode());
+            curr = curr.children.get(ch);
+        }
+        curr.endOfWord = true;
+    }
+
+    boolean search(String input) {
+        TrieNode curr = root;
+        for(char ch : input.toCharArray()) {
+            if(!curr.children.containsKey(ch)) {
+                return false;
+            }
+            curr = curr.children.get(ch);
+        }
+        if(curr.endOfWord) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean startsWith(String input) {
+        TrieNode curr = root;
+        for(char ch : input.toCharArray()) {
+            if(!curr.children.containsKey(ch)) {
+                return false;
+            }
+            curr = curr.children.get(ch);
+        }
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("cat");
+        trie.insert("car");
+        trie.insert("dog");
+
+        System.out.println("Search 'cat': " + trie.search("cat")); // true
+        System.out.println("Search 'car': " + trie.search("car")); // true
+        System.out.println("Search 'dog': " + trie.search("dog")); // true
+        System.out.println("Search 'cart': " + trie.search("cart")); // false
+        System.out.println("startsWith 'ca': " + trie.startsWith("ca")); // true
+        System.out.println("startsWith 'do': " + trie.startsWith("do")); // true
+        System.out.println("startsWith 'ze': " + trie.startsWith("ze")); // false
+    }
+}
